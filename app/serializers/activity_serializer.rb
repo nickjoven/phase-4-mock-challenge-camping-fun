b98ec3edbe01838627acf7d3a85c19eb33cff143
Nicholas <nicholasjoven@gmail.com>
2022-09-07 14:50:19 -0400

    added no of signups to activity serializer

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,3 +1,7 @@
 class ActivitySerializer < ActiveModel::Serializer
-  attributes :id, :name, :difficulty
+  attributes :id, :name, :difficulty, :number_of_signups
+
+  def number_of_signups
+    object.signups.count
+  end
 end
